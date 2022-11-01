d6b7b0c86929160494267d2f9f74c6d0c30fadb0
indahnirmalazahranabil <83548180+indahnirmalazahranabil@users.noreply.github.com>
2022-11-01 14:54:33 +0700

    menambahkan setting untuk edit akun

---
+++ app/Views/toko/edit_detail.php
@@ -0,0 +1,67 @@
+<?= $this->extend('templates/index'); ?>
+<?= $this->section('page-content'); ?>
+
+
+<div class="ctnr">
+    <p>Ubah Data Toko</p>
+</div>
+<hr>
+<div class="col-sm-4">
+
+    <form action="#" method="post">
+
+        <div class="form-group">
+            <label>Nama Toko</label>
+            <input type="text" name="nama_toko" list="list" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label>Alamat</label>
+            <input type="text" name="alamat_toko" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label>No.Telepon Toko</label>
+            <input type="text" name="nomor_telepon_toko" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>No.Telepon Kepala Toko</label>
+            <input type="text" name="nomor_telepon_kp_toko" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Kepala Toko</label>
+            <input type="text" name="nama_kp_toko" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>email kepala Toko</label>
+            <input type="text" name="email_kp_toko" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>username kepala Toko</label>
+            <input type="text" name="username_kp_toko" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>password kepala Toko</label>
+            <input type="text" name="password_kp_toko" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>foto kepala Toko</label>
+            <input type="text" name="foto_kp_toko" class="form-control">
+        </div>
+
+        <button type="submit" style="
+                cursor: pointer;
+                border-radius: 10px;
+                background-color: #F4EB93;
+                padding: 5px 30px 5px 30px;
+                font-family: roboto;
+                /* margin-left:30%; */
+                font-size: 15px;
+                color:#171C7B; 
+                border-color:#171C7B;         
+                font-weight: bold;">Save</button>
+    </form>
+
+
+</div>
+<?= $this->endSection(); ?>
